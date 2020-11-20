848ec703636dc76bb29c1935736a1b5193651dca
Sercan Leylek <leylek.sercan@gmail.com>
2020-11-20 14:27:14 +0100

    Create booking grid

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const numOfDays = 10;
 
 function App() {
   const [bookings, setBookings] = useState<IBooking[]>();
-  const [initialDate, setInitialDate] = useState<Date>(new Date());
+  let initialDate = new Date();
 
   useEffect(() => {
     loadBookings();
@@ -27,7 +27,7 @@ function App() {
         bookingArray.push(tuple as IBooking);
       });
     }).then(function() {
-      console.log(bookingArray);
+      //console.log(bookingArray);
       setBookings(bookingArray);
     });
   };
@@ -35,6 +35,37 @@ function App() {
   const returnCollection = (collectionName: string) => {
     const db = fire.firestore();
     return db.collection(collectionName);
+  };
+
+  const renderBookingGrid = () => {
+    const rows:JSX.Element[] = [];
+      console.log("initialDate", initialDate);
+    let currentDate = initialDate;
+    for (let i = 0; i < numOfDays; i++) {
+      rows.push(
+          <div className={"bookingGrid-row"}>
+            <div className={"bookingGrid-row__dateCell"}>{formatDate(currentDate)}</div>
+            <div className={"bookingGrid-row__bookingCell"}></div>
+            <div className={"bookingGrid-row__bookingCell"}></div>
+            <div className={"bookingGrid-row__bookingCell"}></div>
+            <div className={"bookingGrid-row__bookingCell"}></div>
+          </div>
+      );
+      currentDate.setDate(currentDate.getDate() + 1);
+      //console.log("currentDate", currentDate);
+    }
+
+    return (
+        <Card className="bookingGrid">
+          {rows}
+        </Card>
+    );
+  };
+
+  const formatDate = (date: Date): string => {
+      const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(date);
+      const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
+      return mo + " " + da;
   };
 
   return (
@@ -47,7 +78,6 @@ function App() {
             <DatePicker
                 label={""}
                 helpLabel={"Set initial date"}
-                initialDate={initialDate}
                 onBlur={(date) => {
                   console.log(date);
                 }}
@@ -56,7 +86,7 @@ function App() {
             />
           </div>
           <div className="bookingPanel-row">
-            <Card className="bookingGrid"></Card>
+            {renderBookingGrid()}
           </div>
         </section>
       </main>
