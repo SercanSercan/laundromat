756816f8a053d7bdbee04d892aca5afe81858f9c
Sercan Leylek <leylek.sercan@gmail.com>
2020-11-20 11:20:20 +0100

    Establish connection with Cloud Firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import fire from './config/fire';
+
+const collectionName = "gabelsGate1";
 
 function App() {
+
+  const loadBookings = () => {
+    let dbCollection = returnCollection(collectionName);
+    console.log(dbCollection);
+  };
+
+  const returnCollection = (collectionName: string) => {
+    const db = fire.firestore();
+    return db.collection(collectionName);
+  };
+
+  loadBookings();
+
   return (
     <div className="App">
-
+      hello
     </div>
   );
 }
