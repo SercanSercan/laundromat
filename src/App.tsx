8833f3541285298ff5b5e55ca2432d6a43407bbf
Sercan Leylek <leylek.sercan@gmail.com>
2020-11-20 13:44:52 +0100

    Use datepicker for initialDate

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
+import { Card } from '@fremtind/jkl-card-react';
+import { DatePicker } from '@fremtind/jkl-datepicker-react';
+import "@fremtind/jkl-datepicker/datepicker.min.css";
+import "@fremtind/jkl-icon-button/icon-button.min.css";
 import fire from './config/fire';
-import {IBooking} from "./interfaces";
+import { IBooking } from "./interfaces";
 
 const collectionName = "gabelsGate1";
 const numOfDays = 10;
 
 function App() {
   const [bookings, setBookings] = useState<IBooking[]>();
+  const [initialDate, setInitialDate] = useState<Date>(new Date());
 
   useEffect(() => {
     loadBookings();
@@ -37,7 +42,23 @@ function App() {
       <header>
       </header>
       <main>
-
+        <section className="bookingPanel">
+          <div className="bookingPanel-row">
+            <DatePicker
+                label={""}
+                helpLabel={"Set initial date"}
+                initialDate={initialDate}
+                onBlur={(date) => {
+                  console.log(date);
+                }}
+                extended
+                placeholder={"placeholder"}
+            />
+          </div>
+          <div className="bookingPanel-row">
+            <Card className="bookingGrid"></Card>
+          </div>
+        </section>
       </main>
       <footer></footer>
     </div>
